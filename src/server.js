7b359e3341fed133b3c5f32d8f0f4ece5bcb051a
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-04-24 10:00:30 -0400

    added configs for deploy

--- src/server.js
+++ src/server.js
@@ -1,22 +1,18 @@
-const knex = require('knex')
-const app = require('./app')
-const { PORT, DB_URL } = require('./config')
+const knex = require("knex");
+const app = require("./app");
+const { PORT, DATABASE_URL } = require("./config");
 //const FoldersService = require('./folders/folders-service')
 
-
-
 const db = knex({
-    client: 'pg',
-    connection: DB_URL,
-})
+  client: "pg",
+  connection: DATABASE_URL,
+});
 
 // console.log('knex and driver installed correctly');
 // console.log(FoldersService.getAllFolders())
 
-
-app.set('db', db)
-
+app.set("db", db);
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
-})
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
