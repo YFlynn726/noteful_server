aa35e6d381639c6f1b78d387e25fbb888ae31c5b
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-04-24 17:33:14 -0400

    fixed err

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || "postgres://dunder_mifflin@localhost:5432/noteful_app",
-}
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgres://dunder_mifflin@localhost:5432/noteful_app",
+};
