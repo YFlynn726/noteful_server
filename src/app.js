6203fa0c9639c13c4963ed9cf1a229b689f91201
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2020-04-24 21:09:21 -0400

    made changes

--- src/app.js
+++ src/app.js
@@ -11,18 +11,18 @@ const notesRouter = require("./notes/notes-router");
 const app = express();
 
 app.use(
-    morgan(NODE_ENV === "production" ? "tiny" : "common", {
-        skip: () => NODE_ENV === "test",
-    })
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test",
+  })
 );
 app.use(cors());
 app.use(helmet());
 
-app.use("/api/folders", foldersRouter);
+app.use(foldersRouter);
 app.use("/api/notes", notesRouter);
 //app.use("/api/comments", commentsRouter);
 
-app.get("/api/folders", foldersRouter);
+//app.get("/api/folders", foldersRouter);
 app.get("/api/folders/:folder_id", foldersRouter);
 app.post("/api/folders", foldersRouter);
 
@@ -31,18 +31,18 @@ app.get("/api/notes/:note_id", notesRouter);
 app.post("/api/notes", notesRouter);
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+  res.send("Hello, world!");
 });
 
 app.use(function errorHandler(error, req, res, next) {
-    let response;
-    if (NODE_ENV === "production") {
-        response = { error: "Server error" };
-    } else {
-        console.error(error);
-        response = { message: error.message, error };
-    }
-    res.status(500).json(response);
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: "Server error" };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
 });
 
 module.exports = app;
